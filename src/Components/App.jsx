92327ad3202e9f46ae1548951b0d64c47122de3f
ameya-6964 <belvalkar7@gmail.com>
2023-01-04 17:04:23 +0530

    Refractored Properly

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,28 +6,35 @@ import MovieInformation from './MovieInformation/MovieInformation';
 import Movies from './Movies/Movies';
 import Profile from './Profile/Profile';
 import Actors from './Actors/Actors';
-// import useStyles from './styles';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <MovieInformation />
-        </Route>
-        <Route exact path="/actors/:id">
-          <Actors />
-        </Route>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
 export default App;
+
