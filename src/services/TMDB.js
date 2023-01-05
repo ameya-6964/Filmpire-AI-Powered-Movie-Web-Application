dc640bda59c9a3b47b93fd4c63514c43c1081f39
ameya-6964 <belvalkar7@gmail.com>
2023-01-05 23:02:00 +0530

    Done With Api

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,1 +1,21 @@
-import { createAPI, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.TMDB_KEY;
+const page = 1;
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get Popular Movies
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+    }),
+  }),
+});
+
+export const {
+  useGetMoviesQuery,
+} = tmdbApi;
